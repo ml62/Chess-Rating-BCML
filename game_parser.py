e16325002488a0e68e05c781ace16924877e6db4
chandarb <chandarbharatk@gmail.com>
2014-12-17 10:31:03 -0600

    added code to pass game to stockfish

--- game_parser.py
+++ game_parser.py
@@ -1,6 +1,7 @@
 import chess.pgn
+import subprocess, time
 
-
+multipv = 5
 pgn = open("data.pgn")
 
 def read_game(pgn):
@@ -15,3 +16,38 @@ def game_in_uci(game):
 		node = node.variations[0]
 		moves.append(node.move.uci())
 	return moves
+
+engine = subprocess.Popen(
+	'stockfish-x64.exe',
+	universal_newlines=True,
+	stdin=subprocess.PIPE,
+	stdout=subprocess.PIPE,
+)
+
+def put(command):
+	engine.stdin.write(command+"\n")
+
+def get():
+	engine.stdin.write('isready\n')
+	while True:
+		text=engine.stdout.readline().strip()
+		if text == 'readyok':
+			break
+		if text != ""
+			print('\t' + text)
+
+def run_game(moves):
+	for i in range(len(moves)):		
+		get()
+		put('uci')
+		get()
+		put('setoption name multipv value ' + multipv)
+		get()
+		put('ucinewgame')
+		get()
+		put('position startpos moves' + moves[0:i].join(" "))
+		get()
+		put('go infinite')
+		time.sleep(3)
+		get()
+		put('stop')
